900f1ee43604f6af9515ee06a913f8edd4b99557
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-06-24 22:47:31 +0200

    create image gallery for quiz images

---
+++ src/components/QuizSection.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import { quizData } from "./quizData"
+
+const QuizGallery = () => {
+const [index, setIndex] = useState(0);
+
+const handleNextClick = () => {
+  setIndex(index + 1)
+  if (index === (quizData.length -1)) {
+    setIndex(0)
+  }
+}
+const handlePrevClick = () => {
+  setIndex(index - 1)
+  if (index < 1 ) {
+    setIndex(quizData.length -1)
+  }
+}
+const quiz = quizData[index]
+
+return (
+      <div className="img-carousel">
+        <button onClick={handlePrevClick}>Next image</button>
+        <div className="img-1"><h1>{quiz.image}, {quizData.length}</h1></div>
+        <button onClick={handleNextClick}>Next image</button>
+      </div>
+  )
+}
+
+const Quiz = () => {
+  return (
+    <div className="section1">
+      <h1>Webshop</h1>
+      <QuizGallery />
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum, in, vel sed quia ad culpa magni, quam quisquam repellendus quasi? At nemo perferendis distinctio amet, a iusto molestiae ex.</p>
+    </div>
+  )
+}
+
+export default Quiz
