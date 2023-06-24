4750c94b6ee3fd6f5a751ab5276f7e4bac0c5546
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-06-24 22:22:22 +0200

    add example img divs and content for styling

--- src/components/Section1.tsx
+++ src/components/Section1.tsx
@@ -1,8 +1,13 @@
 const Section1 = () => {
   return (
     <div className="section1">
-    <h1>Section 1</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum, in, vel sed quia ad culpa magni, quam quisquam repellendus quasi? At nemo perferendis distinctio amet, a iusto molestiae ex.</p>
+      <div className="img-carousel">
+        <div className="img-1">Image 1</div>
+        <div className="img-2">Image 2</div>
+        <div className="img-3">Image 3</div>
+      </div>
+      <h1>Webshop</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum, in, vel sed quia ad culpa magni, quam quisquam repellendus quasi? At nemo perferendis distinctio amet, a iusto molestiae ex.</p>
   </div>
   )
 }
