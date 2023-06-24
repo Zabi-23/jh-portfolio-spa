d8468894a4c83c3fccf6ab2e815ad8fc72a14c8a
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-06-24 22:21:54 +0200

    create card effect for stacked images

--- src/components/Section2.tsx
+++ src/components/Section2.tsx
@@ -1,8 +1,13 @@
 const Section2 = () => {
   return (
     <div className="section2">
-    <h1>Section 2</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum, in, vel sed quia ad culpa magni, quam quisquam repellendus quasi? At nemo perferendis distinctio amet, a iusto molestiae ex.</p>
+      <div className="img-cards">
+        <div className="img-card1">Image 1</div>
+        <div className="img-card2">Image 2</div>
+        <div className="img-card3">Image 3</div>
+      </div>
+      <h1>Section 2</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum, in, vel sed quia ad culpa magni, quam quisquam repellendus quasi? At nemo perferendis distinctio amet, a iusto molestiae ex.</p>
   </div>
   )
 }
