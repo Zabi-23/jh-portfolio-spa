e1c2f0c743b17f3039614cb41de0c7f34be71463
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-08-03 22:12:32 +0200

    update correct title for component

--- src/components/QuizSection.tsx
+++ src/components/QuizSection.tsx
@@ -30,7 +30,7 @@ return (
 const Quiz = () => {
   return (
     <div className="section section1">
-      <h1>Webshop</h1>
+      <h1>Quiz App for Educators</h1>
       <QuizGallery />
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat eum, in, vel sed quia ad culpa magni, quam quisquam repellendus quasi? At nemo perferendis distinctio amet, a iusto molestiae ex.</p>
     </div>
